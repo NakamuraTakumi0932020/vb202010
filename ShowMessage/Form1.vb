365f6f779c6cf0f2aa74fec28700ea1a0c09334b
Takumi Nakamura <nakamuratakumi093@gmail.com>
2020-10-06 14:21:09 +0900

    新規登録

---
+++ ShowMessage/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class frmShowMessage
+    Private Sub btnExit_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub ShowMessage(sender As Object, e As EventArgs) Handles btnMessage.Click
+        lblMessage.Text = "Hello VB!"
+    End Sub
+
+    Private Sub ExitProc(sender As Object, e As EventArgs) Handles btnExit.Click
+        Application.Exit()
+    End Sub
+End Class
